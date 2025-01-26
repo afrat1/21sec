80ea8ca00511efbf249ffa268d46b4d08906691a
afrat1 <afkaya48300@gmail.com>
2025-01-26 21:13:33 +0300

    feat: added sign-in page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain" />
+        <View className="px-10">
+        <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To 21SEC
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Goals</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to 21sec with Google
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       fontFamily: {
-        "Rubik-Bold": ["Rubik-Bold", "sans-serif"],
-        "Rubik-ExtraBold": ["Rubik-ExtraBold", "sans-serif"],
-        "Rubik-Light": ["Rubik-Light", "sans-serif"],
-        "Rubik-Medium": ["Rubik-Medium", "sans-serif"],
-        "Rubik-Regular": ["Rubik-Regular", "sans-serif"],
-        "Rubik-SemiBold": ["Rubik-SemiBold", "sans-serif"],
-      }, 
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+      },
       colors: {
         primary: {
           100: "#0061FF0A",
@@ -33,4 +32,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
